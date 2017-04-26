c627c7d2d553b7e5929a14e668b7ab45d7fda61d
Misako Tateiwa <mtateiwa@m08.itscom.net>
2017-04-26 10:35:30 +0900

    fix dialog open/close method to use javascript DOM API

--- src/Dialog.js
+++ src/Dialog.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react'
 import './Dialog.css'
 import PropTypes from 'prop-types'
+import ReactDom from 'react-dom'
 
 class Dialog extends Component {
   constructor(props){
     super(props)
     this.state = {isOpen: false}
   }
+  openDialog(){
+    ReactDom.findDOMNode(this.refs.dialog).showModal()
+  }
+  closeDialog(){
+    ReactDom.findDOMNode(this.refs.dialog).close()
+  }
   render(){
     return(
-      <dialog open={this.props.isOpen}>
+      <dialog ref='dialog'>
         <label>todo</label><input type='text' />
         <button onClick={this.props.closeDialog}>Close Dialog</button>
       </dialog>
     )
   } 
+  componentDidUpdate(){
+    if(this.props.isOpen){
+      this.openDialog()
+    } else {
+      this.closeDialog()
+    }
+  }
 }
 
 Dialog.propTypes = {
